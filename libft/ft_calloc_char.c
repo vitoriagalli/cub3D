b769e118321daca915f55b412f0351d20fea6e2b
vscabell <vscabell@student.42sp.org.br>
2020-07-28 16:38:24 +0200

    ft_calloc_char fix

--- libft/ft_calloc_char.c
+++ libft/ft_calloc_char.c
@@ -6,7 +6,7 @@
 /*   By: vscabell <vscabell@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/08 10:10:34 by vscabell          #+#    #+#             */
-/*   Updated: 2020/06/10 01:23:58 by vscabell         ###   ########.fr       */
+/*   Updated: 2020/07/28 16:36:08 by vscabell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	*ft_calloc_char(size_t count, char c)
 {
-	void	*ptr;
-	size_t	mem;
+	unsigned char	*ptr;
+	size_t			mem;
 
 	mem = count * sizeof(char);
 	if (!(ptr = malloc(mem + 1)))
 		return (NULL);
 	ft_memset(ptr, c, mem);
-	return (ptr);
+	ptr[mem] = '\0';
+	return ((void *)ptr);
 }
